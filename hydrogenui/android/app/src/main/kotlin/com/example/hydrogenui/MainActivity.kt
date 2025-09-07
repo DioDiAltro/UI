3bc16d532ab59fb00ec0ecef57169706b2876f8f
DioDiAltro <127589264+DioDiAltro@users.noreply.github.com>
2025-09-07 15:45:21 +0200

    cristo

--- hydrogenui/android/app/src/main/kotlin/com/example/hydrogenui/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hydrogenui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- hydrogenui/lib/core/theme/palette.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Palette {
-  static const Color primaryColor = Color(0xFFDC5741);
-  static const Color secondaryColor = Color(0xFF8E665E);
-  static const Color scaffoldBackgroundColor = Color(0xFF111312);
-  static const Color labelColor = Color(0xFF787878);
-}
