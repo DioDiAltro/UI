8c7a98bf4c1bdaddf4c4186488f3a4e232386d1a
DioDiAltro <127589264+DioDiAltro@users.noreply.github.com>
2025-09-18 12:39:21 +0200

    minchia

--- hydrogenui/lib/screens/home_page.dart
+++ hydrogenui/lib/screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hydrogenui/widgets/custom_card.dart';
 import 'package:hydrogenui/widgets/terminal_input.dart';
+import 'dart:io';
 
 class ClockController extends GetxController {
   var time = ''.obs;
@@ -44,12 +45,11 @@ class MyHomePage extends StatelessWidget {
                     // Colonna Panel System
                     child: Column(
                       children: [
-                        Expanded(
-                          child: Text(
-                            'PANEL SYSTEM',
-                            style: TextStyle(wordSpacing: 320),
-                          ),
+                        Text(
+                          'PANEL SYSTEM',
+                          style: TextStyle(wordSpacing: 270),
                         ),
+                        _CustomDivider(context),
                         Expanded(
                           child: Card(
                             child: Center(
@@ -86,11 +86,12 @@ class MyHomePage extends StatelessWidget {
                                 'TERMINAL MAIN',
                                 style: TextStyle(wordSpacing: 320),
                               ),
-
+                              _TerminalChoice(platform: _PlatformCheck()),
                               // Widget per selezionare il terminale da usare
                             ],
                           ),
                         ),
+                        _CustomDivider(context),
                         const SizedBox(height: 2),
                         Expanded(child: TerminalInput()),
                       ],
@@ -99,7 +100,16 @@ class MyHomePage extends StatelessWidget {
                   Expanded(
                     flex: 2,
                     // Colonna del Network
-                    child: Column(children: []),
+                    child: Column(
+                      children: [
+                        Text(
+                          'PANEL NETWORK',
+                          style: TextStyle(wordSpacing: 270),
+                        ),
+                        _CustomDivider(context),
+                        Expanded(child: CustomCard(title: "Network Stats")),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -135,4 +145,88 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+int _PlatformCheck() {
+  if (Platform.isWindows) {
+    return 0;
+  } else if (Platform.isLinux) {
+    return 1;
+  } else if (Platform.isMacOS) {
+    return 2;
+  } else if (Platform.isAndroid) {
+    return 3;
+  } else if (Platform.isIOS) {
+    return 4;
+  } else {
+    // print("Sistema operativo sconosciuto: ${Platform.operatingSystem}");
+    return -1;
+  }
+}
+
+class _TerminalController extends GetxController {
+  var selectedTerminal = ''.obs; // valore selezionato
+}
+
+class _TerminalChoice extends StatelessWidget {
+  final int platform;
+  final _TerminalController controller = Get.put(_TerminalController());
+
+  final List<DropdownMenuItem<String>> allTerminal = [
+    DropdownMenuItem(value: 'cmd', child: Text('CMD')), // Solo Windows
+    DropdownMenuItem(
+      value: 'powershell',
+      child: Text('PowerShell'),
+    ), // Solo Windows
+    DropdownMenuItem(value: 'zsh', child: Text('Zsh')), // Solo macOS
+    DropdownMenuItem(value: 'bash', child: Text('Bash')), // Linux, macOS
+    DropdownMenuItem(value: 'sh', child: Text('Sh')), // Linux
+  ];
+
+  List<DropdownMenuItem<String>> get items {
+    switch (platform) {
+      case 0:
+        return allTerminal.sublist(0, 2); // Windows
+      case 1:
+        return allTerminal.sublist(3, 5); // Linux
+      case 2:
+        return allTerminal.sublist(2, 4); // macOS
+      default:
+        return [];
+    }
+  }
+
+  _TerminalChoice({required this.platform});
+
+  @override
+  Widget build(BuildContext context) {
+    // Imposta un valore iniziale se non c'è
+    if (controller.selectedTerminal.value == '' && items.isNotEmpty) {
+      controller.selectedTerminal.value = items.first.value!;
+    }
+
+    return Obx(
+      () => SizedBox(
+        height: 20,
+        child: DropdownButton<String>(
+          underline: SizedBox.shrink(),
+          style: TextStyle(fontSize: 15),
+          value: controller.selectedTerminal.value,
+          items: items,
+          onChanged: (value) {
+            if (value != null) controller.selectedTerminal.value = value;
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Widget _CustomDivider(context) {
+  return Divider(
+    indent: 10,
+    endIndent: 10,
+    color: Theme.of(context).colorScheme.primary,
+    thickness: 2,
+  );
 }
